4861d1de2c38b9d8f21c8bbffab9b9f8ced147b4
Wentao WANG <2906482306@qq.com>
2017-06-06 10:20:21 +0800

    modify description of makeDoor

--- src/main/java/com/pattern/creational/simplefactory/DoorFactory.java
+++ src/main/java/com/pattern/creational/simplefactory/DoorFactory.java
@@ -8,7 +8,7 @@ public class DoorFactory {
      * Make a new door
      * @param  width  width of door
      * @param  height height of door
-     * @return        the new door
+     * @return a new door
      */
     public static Door makeDoor(double width, double height) {
         return new WoodenDoor(width, height);
